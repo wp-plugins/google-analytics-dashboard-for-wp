b07c6d02ecaef6efd68ef6bea986f1587c043d16
deconf <deconf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-13 16:50:08 +0000

    Version 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-dashboard-for-wp@681349 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.2.1/functions.php
@@ -0,0 +1,22 @@
+<?php	
+	function ga_dash_store_token ($token){
+		update_option('ga_dash_token', $token);
+	}		
+	
+	function ga_dash_get_token (){
+
+		if (get_option('ga_dash_token')){
+			return get_option('ga_dash_token');
+		}
+		else{
+			return;
+		}
+	
+	}
+	
+	function ga_dash_reset_token (){
+
+		update_option('ga_dash_token', ""); 
+	
+	}
+?>
