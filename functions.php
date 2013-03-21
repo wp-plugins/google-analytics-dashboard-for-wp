73e0072cd99e85a18866a37280531f5d4e920754
deconf <deconf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-21 16:04:34 +0000

    Version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-dashboard-for-wp/tags/2.0@685274 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,84 @@
+<?php	
+	function ga_dash_store_token ($token){
+		update_option('ga_dash_token', $token);
+	}		
+	
+	function ga_dash_get_token (){
+
+		if (get_option('ga_dash_token')){
+			return get_option('ga_dash_token');
+		}
+		else{
+			return;
+		}
+	
+	}
+	
+	function ga_dash_reset_token (){
+		update_option('ga_dash_token', "");
+		update_option('ga_dash_tableid', "");
+		update_option('ga_dash_tableid_jail', "");
+		update_option('ga_dash_profile_list', "");
+		update_option('ga_dash_access', ""); 		
+	}
+	
+// Get Top Pages
+	function ga_dash_top_pages($service, $projectId, $from, $to){
+		$code="";
+		$metrics = 'ga:pageviews'; 
+		$dimensions = 'ga:pageTitle';
+		try{
+			$data = $service->data_ga->get('ga:'.$projectId, $from, $to, $metrics, array('dimensions' => $dimensions, 'sort' => '-ga:pageviews', 'max-results' => '5', 'filters' => 'ga:pagePath!=/'));	
+		}  
+			catch(exception $e) {
+			echo "<br />ERROR LOG:<br /><br />".$e; 
+		}	
+		
+		$code .= '<ol>';
+		foreach ($data['rows'] as $items){
+			$code .= '<li><i>'.substr(esc_html($items[0]),0,70).'</i> - '.number_format($items[1]).' views</li>';
+		}
+		$code .= '</ol>';
+		return $code;
+	}
+	
+// Get Top referrers
+	function ga_dash_top_referrers($service, $projectId, $from, $to){
+		$code="";
+		$metrics = 'ga:visits'; 
+		$dimensions = 'ga:source,ga:medium';
+		try{
+			$data = $service->data_ga->get('ga:'.$projectId, $from, $to, $metrics, array('dimensions' => $dimensions, 'sort' => '-ga:visits', 'max-results' => '6', 'filters' => 'ga:medium==referral'));	
+		}  
+			catch(exception $e) {
+			echo "<br />ERROR LOG:<br /><br />".$e; 
+		}	
+		
+		$code .= '<ul>';
+		foreach ($data['rows'] as $items){
+			$code .= '<li><i>'.esc_html($items[0]).'</i> - '.number_format($items[2]).' visits</li>';
+		}
+		$code .= '</ul>';
+		return $code;
+	}
+
+// Get Top searches
+	function ga_dash_top_searches($service, $projectId, $from, $to){
+		$code="";
+		$metrics = 'ga:visits'; 
+		$dimensions = 'ga:keyword';
+		try{
+			$data = $service->data_ga->get('ga:'.$projectId, $from, $to, $metrics, array('dimensions' => $dimensions, 'sort' => '-ga:visits', 'max-results' => '6', 'filters' => 'ga:keyword!=(not provided);ga:keyword!=(not set)'));	
+		}  
+			catch(exception $e) {
+			echo "<br />ERROR LOG:<br /><br />".$e; 
+		}	
+		
+		$code .= '<ul>';
+		foreach ($data['rows'] as $items){
+			$code .= '<li><i>'.esc_html($items[0]).'</i> - '.number_format($items[1]).' visits</li>';
+		}
+		$code .= '</ul>';
+		return $code;
+	}	
+?>
