6367cfcc36ad3dc94d6a4272b4500d4e7a747cfc
deconf <deconf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-10 07:54:56 +0000

    Preparing a new release
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-dashboard-for-wp@695063 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/ga_dash.php
+++ trunk/ga_dash.php
@@ -4,7 +4,7 @@ Plugin Name: Google Analytics Dashboard for WP
 Plugin URI: http://www.deconf.com
 Description: This plugin will display Google Analytics data and statistics into Admin Dashboard. 
 Author: Deconf.com
-Version: 3.3.3
+Version: 3.3.4
 Author URI: http://www.deconf.com
 */  
 
