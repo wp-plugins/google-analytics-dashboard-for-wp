ac1b78fa3751b6391d15684323a783edbaccba37
deconf <deconf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-23 17:34:57 +0000

    - added google analytics real-time support - new date ranges: Today, Yesterday, Last 30 Days and Last 90 Days
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-dashboard-for-wp@777414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/functions.php
+++ trunk/functions.php
@@ -112,16 +112,49 @@
 	function ga_dash_store_token ($token){
 		update_option('ga_dash_token', $token);
 	}		
-	
-	function ga_dash_get_token (){
 
+	function ga_dash_store_refreshtoken ($refresh_token){
+		update_option('ga_dash_refresh_token', $refresh_token);
+	}		
+	
+	function ga_dash_get_refreshtoken (){
+		if (get_option('ga_dash_refresh_token')){
+			return get_option('ga_dash_refresh_token');
+		}
+		else{
+			return;
+		}
+	}
+
+	function ga_dash_get_token (){
 		if (get_option('ga_dash_token')){
 			return get_option('ga_dash_token');
 		}
 		else{
 			return;
 		}
+	}
 	
+	function ga_dash_refresh_token ($client){
+		$transient = get_transient("ga_dash_token_expire");
+		if ( empty( $transient ) ){
+			
+			if (!ga_dash_get_refreshtoken()){
+				$google_token = json_decode(ga_dash_get_token());
+				ga_dash_store_refreshtoken ($google_token->refresh_token);
+				$client->refreshToken($google_token->refresh_token);
+			}else{
+				$client->refreshToken(ga_dash_get_refreshtoken());
+			}
+			
+			$token=$client->getAccessToken();
+			$google_token = json_decode($token);
+			set_transient( "ga_dash_token_refresh", $token, $google_token->expires_in);
+			ga_dash_store_token($token);
+			return $token;
+		}else{
+			return $token;
+		}	
 	}
 	
 	function ga_dash_reset_token (){
@@ -354,6 +387,214 @@
 		}
 		if (!get_option('ga_target_number') AND get_option('ga_target_geomap')){
 			update_option('ga_target_number', "10");
-		}		
+		}
+		if (!get_option('ga_realtime_pages')){
+			update_option('ga_realtime_pages', "10");
+		}
+		
 	}
+	function ga_realtime($devkey,$path){
+		$token=json_decode(get_option('ga_dash_token'));
+		$url=$path."?access_token=".($token->access_token)."&key=$devkey";
+
+		$code='
+
+		<script type="text/javascript">
+
+		var focusFlag = 1;
+
+		jQuery(document).ready(function(){
+			jQuery(window).bind("focus",function(event){
+				focusFlag = 1;
+			}).bind("blur", function(event){
+				focusFlag = 0;
+			});
+		});
+		
+		jQuery(function() {
+			jQuery( document ).tooltip();
+		});
+		
+		function onlyUniqueValues(value, index, self) { 
+			return self.indexOf(value) === index;
+		 }
+
+		function countvisits(data, searchvalue) { 
+			var count = 0;
+			for ( var i = 0; i < data["rows"].length; i = i + 1 ) {
+				if (jQuery.inArray(searchvalue, data["rows"][ i ])>-1){
+					count += parseInt(data["rows"][ i ][6]);
+				}
+ 			}
+			return count;
+		 }
+		
+		function gadash_generatetooltip(data) {
+			var count = 0;
+			var table = "";
+			for ( var i = 0; i < data.length; i = i + 1 ) {
+					count += parseInt(data[ i ].count);
+					table += "<tr><td class=\'gadash-pgdetailsl\'>"+data[i].value+"</td><td class=\'gadash-pgdetailsr\'>"+data[ i ].count+"</td></tr>";
+			};
+			if (count){
+				return("<table>"+table+"</table>");
+			}else{
+				return("");
+			}	
+		}
+		
+		function gadash_pagedetails(data, searchvalue) { 
+			var newdata = [];
+			for ( var i = 0; i < data["rows"].length; i = i + 1 ){
+				var sant=1;
+				for ( var j = 0; j < newdata.length; j = j + 1 ){
+					if (data["rows"][i][0]+data["rows"][i][1]+data["rows"][i][2]+data["rows"][i][3]==newdata[j][0]+newdata[j][1]+newdata[j][2]+newdata[j][3]){
+						newdata[j][6] = parseInt(newdata[j][6]) + parseInt(data["rows"][i][6]);
+						sant = 0;
+					}
+				}
+				if (sant){
+					newdata.push(data["rows"][i].slice());
+				}
+			}
+
+			var countrfr = 0;
+			var countkwd = 0;
+			var countdrt = 0;
+			var countscl = 0;
+			var tablerfr = "";
+			var tablekwd = "";
+			var tablescl = "";
+			var tabledrt = "";
+			for ( var i = 0; i < newdata.length; i = i + 1 ) {
+				if (newdata[i][0] == searchvalue){
+					var pagetitle = newdata[i][5];
+					switch (newdata[i][3]){
+						case "REFERRAL": 	countrfr += parseInt(newdata[ i ][6]);
+											tablerfr +=	"<tr><td class=\'gadash-pgdetailsl\'>"+newdata[i][1]+"</td><td class=\'gadash-pgdetailsr\'>"+newdata[ i ][6]+"</td></tr>";
+											break;
+						case "ORGANIC": 	countkwd += parseInt(newdata[ i ][6]);
+											tablekwd +=	"<tr><td class=\'gadash-pgdetailsl\'>"+newdata[i][2]+"</td><td class=\'gadash-pgdetailsr\'>"+newdata[ i ][6]+"</td></tr>";
+											break;
+						case "SOCIAL": 		countscl += parseInt(newdata[ i ][6]);
+											tablescl +=	"<tr><td class=\'gadash-pgdetailsl\'>"+newdata[i][1]+"</td><td class=\'gadash-pgdetailsr\'>"+newdata[ i ][6]+"</td></tr>";
+											break;											
+						case "DIRECT": 		countdrt += parseInt(newdata[ i ][6]);
+											break;											
+					};
+				};
+ 			};
+			if (countrfr){
+				tablerfr = "<table><tr><td>'.__("REFERRALS",'ga-dash').' ("+countrfr+")</td></tr>"+tablerfr+"</table><br />";
+			}
+			if (countkwd){	
+				tablekwd = "<table><tr><td>'.__("KEYWORDS",'ga-dash').' ("+countkwd+")</td></tr>"+tablekwd+"</table><br />";
+			}
+			if (countscl){
+				tablescl = "<table><tr><td>'.__("SOCIAL",'ga-dash').' ("+countscl+")</td></tr>"+tablescl+"</table><br />";			
+			}	
+			if (countdrt){
+				tabledrt = "<table><tr><td>'.__("DIRECT",'ga-dash').' ("+countdrt+")</td></tr></table><br />";				
+			}	
+			return ("<p><center><strong>"+pagetitle+"</strong></center></p>"+tablerfr+tablekwd+tablescl+tabledrt);
+		 }		 
+		 
+		 function online_refresh(){
+			if (focusFlag){
+			jQuery.getJSON("'.$url.'", function(data){
+				if (data["totalsForAllResults"]["ga:activeVisitors"]!==document.getElementById("gadash-online").innerHTML){
+					jQuery("#gadash-online").fadeOut("slow");
+					jQuery("#gadash-online").fadeOut(500);
+					jQuery("#gadash-online").fadeOut("slow", function() {
+						if ((parseInt(data["totalsForAllResults"]["ga:activeVisitors"]))<(parseInt(document.getElementById("gadash-online").innerHTML))){
+							jQuery("#gadash-online").css({\'background-color\' : \'#FFE8E8\'});
+						}else{
+							jQuery("#gadash-online").css({\'background-color\' : \'#E0FFEC\'});
+						}	
+						document.getElementById("gadash-online").innerHTML = data["totalsForAllResults"]["ga:activeVisitors"];
+					});
+					jQuery("#gadash-online").fadeIn("slow");
+					jQuery("#gadash-online").fadeIn(500);
+					jQuery("#gadash-online").fadeIn("slow", function() {
+						jQuery("#gadash-online").css({\'background-color\' : \'#F8F8F8\'});
+					});
+				};
+				
+				var pagepath = [];
+				var referrals = [];
+				var keywords = [];
+				var social = [];
+				var visittype = [];
+				for ( var i = 0; i < data["rows"].length; i = i + 1 ) {
+					pagepath.push( data["rows"][ i ][0] );
+					if (data["rows"][i][3]=="REFERRAL"){
+						referrals.push( data["rows"][ i ][1] );
+					}
+					if (data["rows"][i][3]=="ORGANIC"){					
+						keywords.push( data["rows"][ i ][2] );
+					}
+					if (data["rows"][i][3]=="SOCIAL"){					
+						social.push( data["rows"][ i ][1] );
+					}						
+					visittype.push( data["rows"][ i ][3] );
+ 				}
+
+				var upagepath = pagepath.filter(onlyUniqueValues);
+				var upagepathstats = [];
+				for ( var i = 0; i < upagepath.length; i = i + 1 ) {
+					upagepathstats[i]={"pagepath":upagepath[i],"count":countvisits(data,upagepath[i])};
+ 				}
+				upagepathstats.sort( function(a,b){ return b.count - a.count } );				
+				
+				var pgstatstable = "";
+				for ( var i = 0; i < upagepathstats.length; i = i + 1 ) {
+					if (i < '.get_option('ga_realtime_pages').'){
+						pgstatstable += "<tr class=\"gadash-pline\"><td class=\"gadash-pleft\"><a href=\"#\" title=\""+gadash_pagedetails(data, upagepathstats[i].pagepath)+"\">"+upagepathstats[i].pagepath.substring(0,70)+"</a></td><td class=\"gadash-pright\">"+upagepathstats[i].count+"</td></tr>";
+					}	
+ 				}
+				document.getElementById("gadash-pages").innerHTML="<br /><table class=\"gadash-pg\">"+pgstatstable+"</table>";
+				
+				var ureferralsstats = [];
+				var ureferrals = referrals.filter(onlyUniqueValues);
+				for ( var i = 0; i < ureferrals.length; i = i + 1 ) {
+					ureferralsstats[i]={"value":ureferrals[i],"count":countvisits(data,ureferrals[i])};
+ 				}
+				ureferralsstats.sort( function(a,b){ return b.count - a.count } );
+
+				var ukeywordsstats = [];
+				var ukeywords = keywords.filter(onlyUniqueValues);
+				for ( var i = 0; i < ukeywords.length; i = i + 1 ) {
+					ukeywordsstats[i]={"value":ukeywords[i],"count":countvisits(data,ukeywords[i])};
+ 				}
+				ukeywordsstats.sort( function(a,b){ return b.count - a.count } );				
+
+				var usocialstats = [];
+				var usocial = social.filter(onlyUniqueValues);
+				for ( var i = 0; i < usocial.length; i = i + 1 ) {
+					usocialstats[i]={"value":usocial[i],"count":countvisits(data,usocial[i])};
+ 				}
+				usocialstats.sort( function(a,b){ return b.count - a.count } );
+				
+				var uvisittype = ["REFERRAL","ORGANIC","SOCIAL"];
+				document.getElementById("gadash-tdo-right").innerHTML = "<span class=\"gadash-bigtext\"><a href=\"#\" title=\""+gadash_generatetooltip(ureferralsstats)+"\">"+\''.__("REFERRAL",'ga-dash').'\'+"</a>: "+countvisits(data,uvisittype[0])+"</span><br /><br />";
+				document.getElementById("gadash-tdo-right").innerHTML += "<span class=\"gadash-bigtext\"><a href=\"#\" title=\""+gadash_generatetooltip(ukeywordsstats)+"\">"+\''.__("ORGANIC",'ga-dash').'\'+"</a>: "+countvisits(data,uvisittype[1])+"</span><br /><br />";
+				document.getElementById("gadash-tdo-right").innerHTML += "<span class=\"gadash-bigtext\"><a href=\"#\" title=\""+gadash_generatetooltip(usocialstats)+"\">"+\''.__("SOCIAL",'ga-dash').'\'+"</a>: "+countvisits(data,uvisittype[2])+"</span><br /><br />";
+
+				var uvisitortype = ["DIRECT","NEW","RETURNING"];
+				document.getElementById("gadash-tdo-rights").innerHTML = "<span class=\"gadash-bigtext\">"+\''.__("DIRECT",'ga-dash').'\'+": "+countvisits(data,uvisitortype[0])+"</span><br /><br />";
+				document.getElementById("gadash-tdo-rights").innerHTML += "<span class=\"gadash-bigtext\">"+\''.__("NEW",'ga-dash').'\'+": "+countvisits(data,uvisitortype[1])+"</span><br /><br />";
+				document.getElementById("gadash-tdo-rights").innerHTML += "<span class=\"gadash-bigtext\">"+\''.__("RETURNING",'ga-dash').'\'+": "+countvisits(data,uvisitortype[2])+"</span><br /><br />";
+				
+				if (!data["totalsForAllResults"]["ga:activeVisitors"]){
+					location.reload();
+				}
+
+			});
+	   };
+	   };	   
+	   online_refresh();
+	   setInterval(online_refresh, 20000);
+	   </script>';
+	return $code;
+}	
 ?>
