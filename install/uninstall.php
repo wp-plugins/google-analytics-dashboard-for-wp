0b4f0019616c0ffbadec539bc076d9cbbdbbf1b7
deconf <deconf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-23 20:22:25 +0000

    - deleting refresh token transient on uninstall
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-dashboard-for-wp/trunk@880562 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- install/uninstall.php
+++ install/uninstall.php
@@ -6,5 +6,6 @@ Class GADASH_Uninstall{
 		$sqlquery = $wpdb->query ( "DELETE FROM $wpdb->options WHERE option_name LIKE '_transient_timeout_gadash%%'" );		
 		delete_option('gadash_options');
 		delete_option('gadash_lasterror');
+		delete_transient ( 'ga_dash_refresh_token' );
 	}
 }
