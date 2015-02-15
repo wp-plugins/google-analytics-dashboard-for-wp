9dbfe2e3585d978a4982a2fb9d375c73b36d7772
deconf <deconf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-15 21:22:41 +0000

    Version 4.4.6
    git-svn-id: https://plugins.svn.wordpress.org/google-analytics-dashboard-for-wp/tags/4.4.6@1090730 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tools/src/Google/Auth/Abstract.php
+++ tools/src/Google/Auth/Abstract.php
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require_once "Google/Http/Request.php";
+require_once realpath(dirname(__FILE__) . '/../../../autoload.php');
 
 /**
  * Abstract class for the Authentication in the API client
@@ -25,16 +25,16 @@ require_once "Google/Http/Request.php";
 abstract class Google_Auth_Abstract
 {
 
-    /**
-     * An utility function that first calls $this->auth->sign($request) and then
-     * executes makeRequest() on that signed request.
-     * Used for when a request
-     * should be authenticated
-     *
-     * @param Google_Http_Request $request            
-     * @return Google_Http_Request $request
-     */
-    abstract public function authenticatedRequest(Google_Http_Request $request);
+  /**
+   * An utility function that first calls $this->auth->sign($request) and then
+   * executes makeRequest() on that signed request.
+   * Used for when a request
+   * should be authenticated
+   *
+   * @param Google_Http_Request $request          
+   * @return Google_Http_Request $request
+   */
+  abstract public function authenticatedRequest(Google_Http_Request $request);
 
-    abstract public function sign(Google_Http_Request $request);
+  abstract public function sign(Google_Http_Request $request);
 }
--- tools/src/Google/Cache/Null.php
+++ tools/src/Google/Cache/Null.php
@@ -14,8 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require_once "Google/Cache/Abstract.php";
-require_once "Google/Cache/Exception.php";
+require_once realpath(dirname(__FILE__) . '/../../../autoload.php');
 
 /**
  * A blank storage class, for cases where caching is not
@@ -24,32 +23,32 @@ require_once "Google/Cache/Exception.php";
 class Google_Cache_Null extends Google_Cache_Abstract
 {
 
-    public function __construct(Google_Client $client)
-    {}
+  public function __construct(Google_Client $client)
+  {}
 
-    /**
-     * @inheritDoc
-     */
-    public function get($key, $expiration = false)
-    {
-        return false;
-    }
+  /**
+   * @inheritDoc
+   */
+  public function get($key, $expiration = false)
+  {
+    return false;
+  }
 
-    /**
-     * @inheritDoc
-     */
-    public function set($key, $value)
-    {
-        // Nop.
-    }
+  /**
+   * @inheritDoc
+   */
+  public function set($key, $value)
+  {
+    // Nop.
+  }
 
-    /**
-     * @inheritDoc
-     *
-     * @param String $key            
-     */
-    public function delete($key)
-    {
-        // Nop.
-    }
+  /**
+   * @inheritDoc
+   *
+   * @param String $key          
+   */
+  public function delete($key)
+  {
+    // Nop.
+  }
 }
